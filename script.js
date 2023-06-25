5bc1663797d2d2d3697b1c1687bda8f03de1abf5
Adam <acho087@gmail.com>
2023-06-25 11:34:54 +0100

    fixed toggle

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 console.log('hello');
 
-toggles = document.querySelector(".toggles");
-  toggle = document.querySelector(".toggle");
+// toggles = document.querySelector(".toggles");
+//   toggle = document.querySelector(".toggle");
+//   toggle.addEventListener("click", () => 
+//     toggles.classList.toggle("dark"));
+//     document.getElementsById('theme').href = 'dark.css';
 
-  toggle.addEventListener("click", () => 
-    toggles.classList.toggle("dark"));
+//   toggle.addEventListener("click", () => toggle.classList.toggle("active"));
 
-  toggle.addEventListener("click", () => toggle.classList.toggle("active"))
+// function setTheme(mode){
+//   if(mode = 'dark'){
+//     document.getElementsByClassName('dark').href = 'dark.css'
+//   }
+// }
+const toggle = document.querySelector(".toggle");
+const themeLink = document.getElementById('theme');
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("dark");
+  
+  if (toggle.classList.contains("dark")) {
+    themeLink.href = "dark.css";
+    
+  } else {
+    themeLink.href = "default.css";
+  }
+});
+
