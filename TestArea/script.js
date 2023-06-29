094c71965cd2e67192d662203786eb120d98b3a6
Adam <acho087@gmail.com>
2023-06-29 21:18:26 +0100

    solidworks

--- TestArea/script.js
+++ TestArea/script.js
@@ -1,25 +1,24 @@
-// Get the modal element
-const modal = document.getElementById("modal");
+function openModal(index) {
+  const dialog = document.getElementById('modal');
+  const image = dialog.querySelector('img');
+  const title = dialog.querySelector('h2');
+  const content = dialog.querySelector('p');
 
-// Get the button that opens the modal
-const openModalBtn = document.getElementById("openModal");
-
-// Get the button that closes the modal
-const closeModalBtn = document.getElementById("closeModal");
-
-// Open the modal when the button is clicked
-openModalBtn.addEventListener("click", function() {
-  modal.showModal();
-});
-
-// Close the modal when the close button is clicked
-closeModalBtn.addEventListener("click", function() {
-  modal.close();
-});
-
-// Close the modal when clicking outside of it
-modal.addEventListener("click", function(event) {
-  if (event.target === modal) {
-    modal.close();
+  // Set the image source, title, and content based on the index
+  if (index === 1) {
+    image.src = 'images/skills/solidworks.png';
+    title.textContent = 'SolidWorks';
+    content.textContent = 'SolidWorks is a powerful 3D computer-aided design (CAD) software...';
+  } else if (index === 2) {
+    image.src = 'images/skills/PTC.png';
+    title.textContent = 'PTC Creo';
+    content.textContent = 'Creo something something...';
   }
-});
+
+  dialog.style.display = 'flex';
+}
+
+function closeModal() {
+  const dialog = document.getElementById('modal');
+  dialog.style.display = 'none';
+}
